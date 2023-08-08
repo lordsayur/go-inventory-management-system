74383cf4ba5e22efabbc4cd0b073cdfa40bc4449
Omar <38073753+lordsayur@users.noreply.github.com>
2023-08-08 15:48:38 +0000

    ✨ add NuxtLabs UI

--- ui/app.vue
+++ ui/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Inventory Management System</h1>
+    <UTable :rows="people" />
   </div>
 </template>
+
+<script setup>
+const people = [
+  {
+    id: 1,
+    name: "Laptop",
+    description: "Dell",
+    quantity: 1,
+    price: 1.2,
+  },
+];
+</script>
