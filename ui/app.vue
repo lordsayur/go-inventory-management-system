626aec8a7e57426ba75bf1ad946f735c7bb9c74e
omar ibrahim <omar.ibrahim@dynamiktechnologies.com.bn>
2023-08-09 14:41:54 +0800

    ✨ update ui

--- ui/app.vue
+++ ui/app.vue
@@ -1,18 +1,52 @@
 <template>
-  <div>
+  <div v-if="items.length > 0">
     <h1>Inventory Management System</h1>
-    <UTable :rows="people" />
+    <form @submit.prevent="onSubmitHandler(newItem)">
+      <button>Add Item</button>
+    </form>
+    <UTable :rows="items" />
   </div>
 </template>
 
 <script setup>
-const people = [
-  {
-    id: 1,
-    name: "Laptop",
-    description: "Dell",
-    quantity: 1,
-    price: 1.2,
-  },
-];
+const baseUrl = "http://localhost:8080";
+
+const items = ref([]);
+const newItem = ref({
+  name: "Item 1",
+  description: "Description",
+  quantity: 1,
+  price: 1.3,
+});
+
+onMounted(async () => {
+  getAllItems();
+});
+
+async function onSubmitHandler(item) {
+  await createItem(item);
+  await getAllItems();
+}
+
+async function getAllItems(sortField = "name", sortOrder = "asc") {
+  try {
+    const { data } = await useFetch(
+      `${baseUrl}/items?sortField=${sortField}&sortOrder=${sortOrder}`
+    );
+    items.value = data.value;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createItem(newItem) {
+  try {
+    const { data } = await useFetch(`${baseUrl}/items`, {
+      method: "POST",
+      body: newItem,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
